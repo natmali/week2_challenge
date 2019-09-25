348616dab85a29a8dc572d2643079e7fe7004ede
nAtnAt' raspberry <natnatraspberry@nAtnAts-MacBook-Air.local>
2019-09-25 14:00:51 +0800

    Remote Repository Push Check

--- challenge.R
+++ challenge.R
@@ -6,4 +6,7 @@ BOM_data
 BOM_stations <- read_csv("data/BOM_stations.csv")
 BOM_stations
 
+## Questions
 
+# For each station, how many days have a minimum temperature, a maximum temperature 
+# and a rainfall measurement recorded?
