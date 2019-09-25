bad431479a8c879ea01336df0e1dd83216a66909
nAtnAt' raspberry <natnatraspberry@nAtnAts-MacBook-Air.local>
2019-09-25 14:23:08 +0800

    My result for part one of the challenge

--- challenge.R
+++ challenge.R
@@ -10,3 +10,9 @@ BOM_stations
 
 # For each station, how many days have a minimum temperature, a maximum temperature 
 # and a rainfall measurement recorded?
+
+BOM_data %>% 
+  separate(Temp_min_max, into = c("min", 'max')) %>% # seperating the max and min temperature
+  filter(min != "", max != "", Rainfall != 0) %>%  # filtering for rows that have a min, max and rainfall value
+  group_by(Station_number) %>% # grouping my station number
+  summarise(n_rows = n()) # counting the number of rows in each group
